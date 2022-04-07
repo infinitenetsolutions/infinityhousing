b3c4a18cacf7d28c134f7a2c0aeecd018ab9437f
= <=>
2022-04-07 17:59:28 +0530

    done

--- contact-us.php
+++ contact-us.php
@@ -1,14 +1,14 @@
 <?php include "include/header.php"?>
-<!--=================================b readcrumb -->
+<!--=================================breadcrumb -->
 <div class="bg-light">
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <ol class="breadcrumb mb-0">
                     <li class="breadcrumb-item">
-                        <a href="index-2.html"> <i class="fas fa-home"></i> </a>
+                        <a href="index.php"> <i class="fas fa-home"></i> </a>
                     </li>
-                    <li class="breadcrumb-item"> <i class="fas fa-chevron-right"></i> <a href="#">Pages</a></li>
+                    <!-- <li class="breadcrumb-item"> <i class="fas fa-chevron-right"></i> <a href="#">Pages</a></li> -->
                     <li class="breadcrumb-item active"> <i class="fas fa-chevron-right"></i> <span> Contact us </span></li>
                 </ol>
             </div>
